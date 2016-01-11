f44cf2393ad5951c6d97310ddfb3ba44a0f3ea3c
Spockuto <venkythesupersaiyan@gmail.com>
2016-01-11 21:35:45 +0530

    Validate Url

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var markup = document.documentElement.innerHTML;
-var regex = new RegExp("(?:href=|src=)\"([^\"]+)" , "g") ;
+var regex = new RegExp("(?:src=)\"([^\"]+)" , "g") ;
 var data = []; 
 var temp;
 while (temp = regex.exec(markup)) {
@@ -7,11 +7,24 @@ data.push(temp[1]);
 }
 var filtered = [] ;
 var i = data.length
-var re =/(\b(https?|www):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+console.log(data.length);
+var re = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
     while (i--) {
-        if (!re.exec(data[i])) {
+        if (!re.test(data[i])) {
             filtered.push(data[i]);
         }
     }
 console.log(filtered);
-console.log(data);
+/*function UrlExists(url)
+{
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status;
+}
+
+var i = filtered.length;
+
+while(i--){
+	console.log(UrlExists(filtered[i]));
+}*/
